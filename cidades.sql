1e7540de9385e2cc475687f7ab834fa761b4480f
unknown <diego.reports@gmail.com>
2020-09-11 11:42:56 -0300

    Criando GoogleChart e banco

---
+++ cidades.sql
@@ -0,0 +1,10 @@
+create table cidades (
+	id INT,
+	cidade VARCHAR(50),
+	populacao INT
+);
+insert into cidades (id, cidade, populacao) values (1, 'Jiwei', 480538);
+insert into cidades (id, cidade, populacao) values (2, 'Drohobych', 842315);
+insert into cidades (id, cidade, populacao) values (3, 'Huangyuan Chengguanzhen', 906007);
+insert into cidades (id, cidade, populacao) values (4, 'Kuusjoki', 334278);
+insert into cidades (id, cidade, populacao) values (5, 'Jixiang', 963255);
